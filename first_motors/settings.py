5a6a5e9daa95c979bc55eeb55c1035d910dc1e60
cboy-sd <cboyc222@gmail.com>
2022-05-10 17:05:02 +0200

    static dile

--- first_motors/settings.py
+++ first_motors/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'statics'),
+]
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
