bbfb00d79ccf549ac7f75bcdd6423924330c0847
cboy-sd <cboyc222@gmail.com>
2022-05-10 16:59:53 +0200

    static files comit

--- first_motors/settings.py
+++ first_motors/settings.py
@@ -122,10 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/uploads/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
 
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
